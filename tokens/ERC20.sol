fbadffe95693143a68b52ff2435f21c1e992133e
Fabrizio Fanini <fabrifanini@gmail.com>
2025-01-16 20:42:32 -0300

    Install openzepellin and update ERC20 file

---
+++ tokens/ERC20.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity  >=0.7.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract fabriERC20 is ERC20, Ownable{
+
+    constructor() ERC20("Fabri ERC20", "FE20") Ownable(msg.sender){
+    }
+
+    function mintFabriERC20 (address account, uint256 value) external {
+        _mint(account, value);
+    }
+
+}
